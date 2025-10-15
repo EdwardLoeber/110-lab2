6e6b2d7bc73aae3b2fa054153a81d0a177037b9c
jsandovalortega <jsandovalortega@ucsd.edu>
2025-10-15 12:52:05 -0700

    added main.ts and used printSnacks from snacks.ts

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { printSnacks } from "./snacks"; 
+
+
+let snacks = ['Turbos', 'Popcorn', 'Sour candy', 'Chocolate', 'Cookies'];
+printSnacks(snacks);
--- src/music.ts
+++ src/music.ts
@@ -1,6 +1,6 @@
 const songs: Array<string> = ['Over the Moon-The Marias','Wish-Blood Orange'];
 
-export function printSongs(): void {
+export default function printSongs(): void {
   console.log("Music:")
   songs.forEach((songs, index) => {
     console.log(`${songs}`);
