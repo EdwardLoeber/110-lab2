d9112ef01050c26b705c5f99252fd17c33996a52
EdwardLoeber <eloeber@ucsd.edu>
2025-10-15 13:36:22 -0700

    main and music animation

--- src/music.ts
+++ src/music.ts
@@ -1,7 +1,10 @@
+import { printFunWay } from "./animation";
+
 const songs: Array<string> = ['Over the Moon-The Marias','Wish-Blood Orange'];
 
 export default function printSongs(): void {
-  console.log("Music:")
+  printFunWay('Get Jiggy Baby');
+  console.log("Music:");
   songs.forEach((songs, index) => {
     console.log(`${songs}`);
   });
