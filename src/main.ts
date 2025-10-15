f9f6375d4cc94f0322edc84884eef30f6fda74ff
jsandovalortega <jsandovalortega@ucsd.edu>
2025-10-15 13:21:09 -0700

    added a newline character for better readability

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import printSongs from './music';
 function main(): void {
     printSongs();
     console.log('\n');
+    console.log('\n');
     let snacks = ['Turbos', 'Popcorn', 'Sour candy', 'Chocolate', 'Cookies'];
     printSnacks(snacks);
 }
